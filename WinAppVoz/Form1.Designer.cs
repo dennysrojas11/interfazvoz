66478861bd4a3b4b1ea4dec00c715488c0a622d3
Dennys Rojas <65422210+dennysrojas11@users.noreply.github.com>
2020-11-04 20:38:09 -0500

    Carga del proyecto
    Carga del proyecto de interfaz de voz

---
+++ WinAppVoz/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WinAppVoz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.textBoxVoz = new System.Windows.Forms.TextBox();
+            this.buttonIniciar = new System.Windows.Forms.Button();
+            this.buttonPausar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(237, 85);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(246, 38);
+            this.labelTitulo.TabIndex = 0;
+            this.labelTitulo.Text = "Interfaz de voz";
+            // 
+            // textBoxVoz
+            // 
+            this.textBoxVoz.Location = new System.Drawing.Point(193, 163);
+            this.textBoxVoz.Name = "textBoxVoz";
+            this.textBoxVoz.Size = new System.Drawing.Size(364, 22);
+            this.textBoxVoz.TabIndex = 1;
+            // 
+            // buttonIniciar
+            // 
+            this.buttonIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIniciar.Location = new System.Drawing.Point(193, 256);
+            this.buttonIniciar.Name = "buttonIniciar";
+            this.buttonIniciar.Size = new System.Drawing.Size(113, 52);
+            this.buttonIniciar.TabIndex = 2;
+            this.buttonIniciar.Text = "Iniciar";
+            this.buttonIniciar.UseVisualStyleBackColor = true;
+            this.buttonIniciar.Click += new System.EventHandler(this.buttonIniciar_Click);
+            // 
+            // buttonPausar
+            // 
+            this.buttonPausar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPausar.Location = new System.Drawing.Point(444, 256);
+            this.buttonPausar.Name = "buttonPausar";
+            this.buttonPausar.Size = new System.Drawing.Size(113, 52);
+            this.buttonPausar.TabIndex = 3;
+            this.buttonPausar.Text = "Pausar";
+            this.buttonPausar.UseVisualStyleBackColor = true;
+            this.buttonPausar.Click += new System.EventHandler(this.buttonPausar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonPausar);
+            this.Controls.Add(this.buttonIniciar);
+            this.Controls.Add(this.textBoxVoz);
+            this.Controls.Add(this.labelTitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.TextBox textBoxVoz;
+        private System.Windows.Forms.Button buttonIniciar;
+        private System.Windows.Forms.Button buttonPausar;
+    }
+}
+
---
+++ WinAppVoz/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace WinAppVoz
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine objVoz = new SpeechRecognitionEngine();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonIniciar_Click(object sender, EventArgs e)
+        {
+            objVoz.SetInputToDefaultAudioDevice();
+            objVoz.LoadGrammar(new DictationGrammar());
+            objVoz.SpeechRecognized += enlazar;
+            objVoz.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        void enlazar(object sender, SpeechRecognizedEventArgs e) {
+            textBoxVoz.Text = e.Result.Text;
+        }
+
+        private void buttonPausar_Click(object sender, EventArgs e)
+        {
+            objVoz.RecognizeAsyncStop();
+        }
+    }
+}
